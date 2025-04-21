3be8c38aebbfb10b6227ed413b623d37be28f799
Keneth Macis <kenethmacis050802@gmail.com>
2025-04-21 15:12:12 -0600

    Planificacion

---
+++ capa_presentacion/Controllers/PlanificacionController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using capa_presentacion.Filters;
+using capa_negocio;
+using capa_entidad;
+
+namespace capa_presentacion.Controllers
+{
+    [VerificarSession]
+    public class PlanificacionController : Controller
+    {        
+        public ActionResult Matriz_de_Integracion()
+        {
+            return View();
+        }
+        public ActionResult Plan_Didactico_Semestral()
+        {
+            return View();
+        }
+        public ActionResult Plan_de_Clases_Diario()
+        {
+            return View();
+        }
+    }
+}
