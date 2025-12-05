b515346864d90df2a4a2c697f7f5155cc158c1f6
Keneth Macis <kenethmacis050802@gmail.com>
2025-12-04 19:57:51 -0600

    sip

---
+++ capa_presentacion/ReportesRDLC/ReporteRDLCViewer.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ReporteRDLCViewer.aspx.cs" Inherits="capa_presentacion.ReportesRDLC.ReporteRDLCViewer" %>
+
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms" Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
+<rsweb:ReportViewer ID="ReportViewer1" runat="server"></rsweb:ReportViewer>
---
+++ capa_presentacion/ReportesRDLC/ReporteRDLCViewer.aspx.cs
@@ -0,0 +1,36 @@
+﻿using capa_entidad;
+using Microsoft.Reporting.WebForms;
+using Microsoft.ReportingServices.ReportProcessing.ReportObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.ScriptManager;
+
+namespace capa_presentacion.ReportesRDLC
+{
+    public partial class ReporteRDLCViewer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack) return;
+            ConfigurarReporte();
+        }
+
+        private void ConfigurarReporte()
+        {
+            string rutaReporteTest1 = "~/ReportesRDLC/Matriz.rdlc";
+
+            ReportViewer1.ProcessingMode = ProcessingMode.Local;
+            ReportViewer1.LocalReport.ReportPath = Server.MapPath(rutaReporteTest1);
+
+            // Configurar parámetros
+            var parametros = new List<Microsoft.Reporting.WebForms.ReportParameter>();
+            
+            ReportViewer1.LocalReport.Refresh();
+        }
+    }
+}
